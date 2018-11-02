31b47e419a09b36a556807557077ea7f5caf36e2
Kaylee Bunce <kaybunce@hotmail.com>
2018-11-02 07:04:50 -0600

    Add styling to splash page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import Main from './Main.js';
 import Splash from './Splash.js';
 import Footer from './Footer.js';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faLightbulb } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faLightbulb);
 
 class App extends Component {
   constructor() {
