f1fb10f329075fa7f415879b812ee97285b91675
Kaylee Bunce <kaybunce@hotmail.com>
2018-11-03 15:30:39 -0600

    Add styling to cards

--- src/CorrectGuess.js
+++ src/CorrectGuess.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import './CorrectGuess.scss'
 import './main.scss';
 
 
 export default class CorrectGuess extends Component {
   render() {
     return (
-      <div> 
-        <p>Correct</p>
+      <div className='correct-guess'> 
+        <div className='wave'></div>
+        <img src='./images/lightbulb.svg' className='lightbulb'/>
+        <p className='correct'>Correct!</p>
+        <button className='next-card-btn'>Next Card</button>
       </div>
     );
   }
--- src/Question.js
+++ src/Question.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import './main.scss';
+import './Question.scss'
 
 
 export default class Question extends Component {
   render() {
     return (
-      <div> 
-        <h3>{this.props.question}</h3>
-        <input type='text' className='guess-input'/>
-        <button onClick={ this.props.showAnswer }>Submit Your Guess</button>
+      <div className='question-container'>
+        <div className='wave'></div> 
+        <h2 className='directions'>What command does the following:</h2>
+        <h3 className='question'>{this.props.question}</h3>
+        <input type='text' className='guess-input' placeholder='Type your answer here'/>
+        <button className='guess-btn' onClick={ this.props.showAnswer }>Submit Your Guess</button>
       </div>
     );
   }
