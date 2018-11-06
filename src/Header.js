82e33efdb06ecc3f417b9a3176140b1062f840ab
Kaylee Bunce <kaybunce@hotmail.com>
2018-11-06 12:16:06 -0700

    Adjust positioning of the logo

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './Header.scss';
 import './main.scss';
 
@@ -7,7 +8,7 @@ export default class Header extends Component {
   render() {
     return (
       <header className='header'>
-        <p>Touch Commands</p>
+        <h1 className='touch-terminal-header'>Touch Terminal <span className='drop-shadow blinking'><FontAwesomeIcon icon="lightbulb"/></span></h1>
       </header>
     );
   }
